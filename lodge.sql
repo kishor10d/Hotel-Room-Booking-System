770e75747fdfe63b5fb42ea09cc77ee618d40e90
kishor10d <kishor10d@gmail.com>
2017-01-20 21:37:51 +0530

    database sql committed

---
+++ lodge.sql
@@ -0,0 +1,258 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 20, 2017 at 05:05 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.5.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lodge`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ldg_floor`
+--
+
+CREATE TABLE `ldg_floor` (
+  `floorId` tinyint(4) NOT NULL,
+  `floorCode` varchar(10) NOT NULL,
+  `floorName` varchar(50) NOT NULL,
+  `floorDescription` text NOT NULL,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  `createdBy` int(11) NOT NULL,
+  `createdDtm` datetime NOT NULL,
+  `updatedBy` int(11) DEFAULT NULL,
+  `updatedDtm` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Floor Table';
+
+--
+-- Dumping data for table `ldg_floor`
+--
+
+INSERT INTO `ldg_floor` (`floorId`, `floorCode`, `floorName`, `floorDescription`, `isDeleted`, `createdBy`, `createdDtm`, `updatedBy`, `updatedDtm`) VALUES
+(1, 'GROUND', 'Ground Floor', '<p>Ground Floor having <strong>6</strong> precious rooms and <strong>2</strong> toilet bathrooms.</p>', 0, 1, '2016-12-31 19:25:12', 1, '2017-01-04 18:03:23'),
+(2, 'FIRST', 'First Floor', '<p>First Floor having <strong>20</strong> Deluxe Single Bed Rooms, <strong>4</strong> Toilets, <strong>4</strong> Bathrooms in each corner in common.</p>', 0, 1, '2017-01-04 18:01:16', 1, '2017-01-04 18:03:00'),
+(3, 'SECOND', 'Second Floor', '<p>Second Floor having <strong>10</strong> double bed rooms with <strong>4</strong> Toilets and <strong>4</strong> Bathrooms in each corner in common.</p>', 0, 1, '2017-01-04 18:02:25', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ldg_lodge`
+--
+
+CREATE TABLE `ldg_lodge` (
+  `lodgeId` int(11) NOT NULL,
+  `lodgeName` varchar(128) NOT NULL,
+  `lodgeAddress` varchar(512) NOT NULL,
+  `lodgeCity` varchar(50) NOT NULL,
+  `lodgeState` varchar(50) NOT NULL,
+  `lodgeCountry` varchar(50) DEFAULT NULL,
+  `lodgePincode` varchar(10) DEFAULT NULL,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  `createdBy` int(11) NOT NULL DEFAULT '0',
+  `createdDtm` datetime NOT NULL,
+  `updatedBy` int(11) DEFAULT NULL,
+  `updatedDtm` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Information of lodge';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ldg_roles`
+--
+
+CREATE TABLE `ldg_roles` (
+  `roleId` tinyint(4) NOT NULL,
+  `role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Information of roles';
+
+--
+-- Dumping data for table `ldg_roles`
+--
+
+INSERT INTO `ldg_roles` (`roleId`, `role`) VALUES
+(1, 'System Administrator'),
+(2, 'Lodge Manager'),
+(3, 'Booker');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ldg_rooms`
+--
+
+CREATE TABLE `ldg_rooms` (
+  `roomId` int(11) NOT NULL,
+  `roomNumber` varchar(50) NOT NULL,
+  `roomSizeId` int(11) NOT NULL COMMENT 'FK : ldg_room_sizes',
+  `floorId` tinyint(4) NOT NULL COMMENT 'FK : ldg_floor',
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  `createdBy` int(11) NOT NULL DEFAULT '0',
+  `createdDtm` datetime NOT NULL,
+  `updatedBy` int(11) DEFAULT NULL,
+  `updateDtm` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Information of rooms';
+
+--
+-- Dumping data for table `ldg_rooms`
+--
+
+INSERT INTO `ldg_rooms` (`roomId`, `roomNumber`, `roomSizeId`, `floorId`, `isDeleted`, `createdBy`, `createdDtm`, `updatedBy`, `updateDtm`) VALUES
+(1, 'G101', 1, 1, 0, 1, '2017-01-10 17:26:22', NULL, NULL),
+(2, 'G102', 1, 1, 0, 1, '2017-01-10 17:29:34', NULL, NULL),
+(3, 'G103', 1, 1, 0, 1, '2017-01-10 17:29:43', NULL, NULL),
+(4, 'G104', 2, 1, 0, 1, '2017-01-18 18:13:45', NULL, NULL),
+(5, 'G105', 2, 1, 0, 1, '2017-01-18 18:15:22', NULL, NULL),
+(6, 'G106', 3, 1, 0, 1, '2017-01-18 18:15:43', NULL, NULL),
+(7, 'G107', 3, 1, 0, 1, '2017-01-18 18:15:52', NULL, NULL),
+(8, 'G108', 4, 1, 0, 1, '2017-01-18 18:16:08', NULL, NULL),
+(9, 'G109', 5, 1, 0, 1, '2017-01-18 18:16:51', NULL, NULL),
+(10, 'G110', 4, 1, 0, 1, '2017-01-18 18:16:35', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ldg_room_sizes`
+--
+
+CREATE TABLE `ldg_room_sizes` (
+  `sizeId` int(11) NOT NULL,
+  `sizeTitle` varchar(512) NOT NULL,
+  `sizeDescription` text NOT NULL,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  `createdBy` int(11) NOT NULL DEFAULT '0',
+  `createdDtm` datetime NOT NULL,
+  `updatedBy` int(11) DEFAULT NULL,
+  `updatedDtm` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Information of room sizes';
+
+--
+-- Dumping data for table `ldg_room_sizes`
+--
+
+INSERT INTO `ldg_room_sizes` (`sizeId`, `sizeTitle`, `sizeDescription`, `isDeleted`, `createdBy`, `createdDtm`, `updatedBy`, `updatedDtm`) VALUES
+(1, 'Deluxe Single Bed Room', '<ol>\r\n<li>Single Bed</li>\r\n<li>Toilet + Bathroom</li>\r\n<li>Hot Water : Morning 5am to 9am</li>\r\n</ol>', 0, 1, '2017-01-04 16:55:01', 1, '2017-01-04 18:06:17'),
+(2, 'Deluxe Double Bed Room', '<ol>\r\n<li>Joint Double Bed</li>\r\n<li>Toilet + Bathroom</li>\r\n<li>Hot Water : Morning 5am to 9am</li>\r\n</ol>', 0, 1, '2017-01-04 18:05:53', 1, '2017-01-04 18:06:34'),
+(3, 'Premium Double Bed Room', '<ol>\r\n<li>Two Single Beds</li>\r\n<li>Toilet + Bathroom</li>\r\n<li>Hot Water : Morning 5am to 9am</li>\r\n</ol>', 0, 1, '2017-01-04 18:07:56', 1, '2017-01-04 18:08:19'),
+(4, 'AC Single Bed Room', '<ol>\r\n<li>Single Bed</li>\r\n<li>Toilet + Bathroom</li>\r\n<li>Hot Water : Morning 5am to 9am</li>\r\n<li>Air Conditioned Room</li>\r\n</ol>', 0, 1, '2017-01-04 18:09:09', 1, '2017-01-04 18:11:05'),
+(5, 'AC Double Bed Room', '<ol>\r\n<li>Double Joint Bed</li>\r\n<li>Toilet + Bathroom</li>\r\n<li>Hot Water : Morning 5am to 9am</li>\r\n<li>Air Conditioned</li>\r\n</ol>', 0, 1, '2017-01-04 18:09:47', 1, '2017-01-04 18:11:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ldg_users`
+--
+
+CREATE TABLE `ldg_users` (
+  `userId` int(11) NOT NULL,
+  `userEmail` varchar(128) NOT NULL,
+  `userPassword` varchar(128) NOT NULL,
+  `userName` varchar(50) NOT NULL,
+  `userPhone` varchar(20) NOT NULL,
+  `userAddress` varchar(1024) NOT NULL,
+  `roleId` tinyint(4) NOT NULL,
+  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  `createdBy` int(11) NOT NULL DEFAULT '0',
+  `createdDtm` datetime NOT NULL,
+  `updatedBy` int(11) DEFAULT NULL,
+  `updatedDtm` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Information of administrative users';
+
+--
+-- Dumping data for table `ldg_users`
+--
+
+INSERT INTO `ldg_users` (`userId`, `userEmail`, `userPassword`, `userName`, `userPhone`, `userAddress`, `roleId`, `isDeleted`, `createdBy`, `createdDtm`, `updatedBy`, `updatedDtm`) VALUES
+(1, 'kishor10d@gmail.com', '$2y$10$qs9FyDuyx9NX72UZpsZdM.BViJkKwRe4ISPx/GIi92id7JxGQDIRa', 'Kishor Mali', '9890098900', 'Pune India', 1, 0, 1, '2017-01-01 00:00:00', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ldg_floor`
+--
+ALTER TABLE `ldg_floor`
+  ADD PRIMARY KEY (`floorId`),
+  ADD UNIQUE KEY `floorCode` (`floorCode`);
+
+--
+-- Indexes for table `ldg_lodge`
+--
+ALTER TABLE `ldg_lodge`
+  ADD PRIMARY KEY (`lodgeId`);
+
+--
+-- Indexes for table `ldg_roles`
+--
+ALTER TABLE `ldg_roles`
+  ADD PRIMARY KEY (`roleId`);
+
+--
+-- Indexes for table `ldg_rooms`
+--
+ALTER TABLE `ldg_rooms`
+  ADD PRIMARY KEY (`roomId`);
+
+--
+-- Indexes for table `ldg_room_sizes`
+--
+ALTER TABLE `ldg_room_sizes`
+  ADD PRIMARY KEY (`sizeId`);
+
+--
+-- Indexes for table `ldg_users`
+--
+ALTER TABLE `ldg_users`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ldg_floor`
+--
+ALTER TABLE `ldg_floor`
+  MODIFY `floorId` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `ldg_lodge`
+--
+ALTER TABLE `ldg_lodge`
+  MODIFY `lodgeId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ldg_roles`
+--
+ALTER TABLE `ldg_roles`
+  MODIFY `roleId` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `ldg_rooms`
+--
+ALTER TABLE `ldg_rooms`
+  MODIFY `roomId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `ldg_room_sizes`
+--
+ALTER TABLE `ldg_room_sizes`
+  MODIFY `sizeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `ldg_users`
+--
+ALTER TABLE `ldg_users`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
