5c012a2a9ef46fdcc45990bba443a030e740cc08
kishor10d <kishor10d@gmail.com>
2017-03-29 21:25:17 +0530

    booking work started

---
+++ application/controllers/booking.php
@@ -0,0 +1,23 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+require APPPATH . '/libraries/BaseController.php';
+
+/**
+ * Class : Booking (BookingController)
+ * Booking Class to control all booking related operations.
+ * @author : Kishor Mali
+ * @version : 1.1
+ * @since : 29 Mar 2017
+ */
+class Booking extends BaseController
+{
+    /**
+     * This is default constructor of the class
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Booking_model', "booking");
+        $this->isLoggedIn();   
+    }
+}
---
+++ application/models/booking_model.php
@@ -0,0 +1,13 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Class : Booking_model 
+ * Booking model to handle database operations related to room booking.
+ * @author : Kishor Mali
+ * @version : 1.1
+ * @since : 29 Mar 2017
+ */
+class Booking_model extends CI_Model
+{
+
+}
